b4f3827721cb3752ada72817c65c04addd01c8ca
noahbr62 <noah.barth@edu.tbz.ch>
2022-01-12 09:25:52 +0100

    Adjusted layout. Added and configured Sensor Used Sensor values for calculations

--- app/src/main/java/com/example/stepbystep/MainActivity.java
+++ app/src/main/java/com/example/stepbystep/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.stepbystep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.health.SystemHealthManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Integer stepCount = 0;
+    private double MagnitudePrevious = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.counter);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent != null){
+                    float xValue = sensorEvent.values[0];
+                    float yValue = sensorEvent.values[1];
+                    float zValue = sensorEvent.values[2];
+
+                    double Magnitude = Math.sqrt(xValue*xValue + yValue*yValue + zValue*zValue);
+                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                    MagnitudePrevious = Magnitude;
+
+                    if (MagnitudeDelta > 6){
+                        stepCount++;
+                    }
+                    editText.setText(stepCount.toString());
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
