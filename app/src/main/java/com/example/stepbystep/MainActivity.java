9fd8be1b55bcfc4434d38aee60cf1191e47c2feb
noahbr62 <noah.barth@edu.tbz.ch>
2022-01-16 12:37:35 +0100

    Implemented Broadcast between service & mainActivity

--- app/src/main/java/com/example/stepbystep/MainActivity.java
+++ app/src/main/java/com/example/stepbystep/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.stepbystep;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.health.SystemHealthManager;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,65 +23,45 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private Integer stepCount = 0;
+    static final String LOG_TAG = "ServiceActivity";
     private double MagnitudePrevious = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = (EditText) findViewById(R.id.counter);
 
-        EditText editText = (EditText) findViewById(R.id.counter);
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        float x_acceleration  = intent.getFloatExtra(MyService.EXTRA_X_VALUE, 0);
+                        float y_acceleration  = intent.getFloatExtra(MyService.EXTRA_Y_VALUE, 0);
+                        float z_acceleration  = intent.getFloatExtra(MyService.EXTRA_Z_VALUE, 0);
+                        System.out.println("x: "+x_acceleration+", y: "+y_acceleration+", z: "+z_acceleration);
+                        double Magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
+                        double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                        MagnitudePrevious = Magnitude;
 
-        SensorEventListener stepDetector = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                if (sensorEvent != null){
-                    float xValue = sensorEvent.values[0];
-                    float yValue = sensorEvent.values[1];
-                    float zValue = sensorEvent.values[2];
-
-                    double Magnitude = Math.sqrt(xValue*xValue + yValue*yValue + zValue*zValue);
-                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
-                    MagnitudePrevious = Magnitude;
-
-                    if (MagnitudeDelta > 5){
-                        stepCount++;
+                        if (MagnitudeDelta > 5){
+                            stepCount++;
+                        }
+                        editText.setText(stepCount+"");
                     }
-                    editText.setText(stepCount.toString());
-                }
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-            }
-        };
-        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+                }, new IntentFilter(MyService.ACTION_SENSOR_BROADCAST)
+        );
     }
 
-    protected void onPause() {
-        super.onPause();
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear();
-        editor.putInt("stepCount", stepCount);
-        editor.apply();
-    }
-
-    protected void onStop() {
-        super.onStop();
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear();
-        editor.putInt("stepCount", stepCount);
-        editor.apply();
-    }
-
+    @Override
     protected void onResume() {
         super.onResume();
-        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
-        stepCount = sharedPreferences.getInt("stepCount", 0);
+        startService(new Intent(this, MyService.class));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopService(new Intent(this, MyService.class));
     }
 }
--- app/src/main/java/com/example/stepbystep/MyService.java
+++ app/src/main/java/com/example/stepbystep/MyService.java
@@ -2,15 +2,61 @@ package com.example.stepbystep;
 
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.IBinder;
 
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import java.util.List;
+
 public class MyService extends Service {
-    public MyService() {
+
+    public static final String
+            ACTION_SENSOR_BROADCAST = MyService.class.getName() + "sensorBroadcast",
+            EXTRA_X_VALUE = "extra_x_value",
+            EXTRA_Y_VALUE = "extra_y_value",
+            EXTRA_Z_VALUE = "extra_z_value";
+
+    private static final int
+            MIN_TIME = 2000,
+            MIN_DISTANCE = 1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                sendBroadcastMessage(sensorEvent);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    private void sendBroadcastMessage(SensorEvent sensorEvent) {
+        if (sensorEvent != null) {
+            Intent intent = new Intent(ACTION_SENSOR_BROADCAST);
+            intent.putExtra(EXTRA_X_VALUE, sensorEvent.values[0]);
+            intent.putExtra(EXTRA_Y_VALUE, sensorEvent.values[1]);
+            intent.putExtra(EXTRA_Z_VALUE, sensorEvent.values[2]);
+            System.out.println("x: "+sensorEvent.values[0]+", y: "+sensorEvent.values[1]+", z: "+sensorEvent.values[2]);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+        }
+    }
+
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 }
